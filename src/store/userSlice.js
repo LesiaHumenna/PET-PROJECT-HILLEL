ee35aa42b023127444664670d56d08c1d1e409c1
Smefinka <smefinka1@gmail.com>
2023-10-15 16:45:10 +0200

    edit

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -2,26 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 import { DB_URL } from "/src/API/firebase";
 
 export const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    isLoggedIn: false,
-    name: "",
-    email: "",
-    userId: "",
-    orders: [],
-    cart: "",
-    user: [],
-  },
-  reducers: {
-    setActiveUser(state, action) {
-      state.isLoggedIn = true;
-      state.name = action.payload.name;
-      state.email = action.payload.email;
-      state.userId = action.payload.userId;
-      state.orders = action.payload.orders;
-      state.cart = action.payload.cart;
+    name: 'user',
+    initialState: {
+        isLoggedIn: false,
+        name: '',
+        email: '',
+        userId: '',
+        orders: [],
+        cart: '',
+        user: []
     },
-
     reducers: {
         setActiveUser(state, action) {
             state.isLoggedIn = true;
@@ -48,38 +38,34 @@ export const userSlice = createSlice({
             
         }               
 
-
-}
-    },
-  },
-);
-//function return another function/ call dispatch in Root
-export const getUserFromDB = (userId) => {
-  return async (dispatch) => {
-    const sendRequest = async () => {
-      const response = await fetch(DB_URL + "/users/" + userId + ".json");
-
-      if (!response.ok) {
-        throw new Error("Cant get user from DB");
-      }
-
-      const data = await response.json();
-      return data;
-    };
-
-    try {
-      const userFromDB = await sendRequest();
-      dispatch(
-        userSlice.actions.setActiveUser({
-          name: userFromDB.name,
-          email: userFromDB.email,
-          userId: userId,
-          cart: userFromDB.cart,
-          orders: userFromDB.orders || [],
-        })
-      );
-    } catch (error) {
-      console.log(error);
     }
-  };
-};
+}
+    )          
+//function return1 another function/ call dispatch in Root
+export const getUserFromDB = (userId) => {
+    return async (dispatch) => {
+        const sendRequest = async () => {
+            const response = await fetch(DB_URL + '/users/' + userId + '.json');
+
+            if (!response.ok) {
+                throw new Error('Cant get user from DB');
+            }
+
+            const data = await response.json();
+            return data;
+        }
+        
+        try {
+            const userFromDB = await sendRequest();
+            dispatch(userSlice.actions.setActiveUser({
+                name: userFromDB.name,
+                email: userFromDB.email,
+                userId: userId,
+                cart: userFromDB.cart,
+                orders: userFromDB.orders || []
+            }));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
