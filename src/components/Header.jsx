1790f8fd9dd4a18611af6b55f60d172472dc5c98
TulupovaI <innatul@ukr.net>
2023-10-19 20:43:22 +0300

    burger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,53 @@
-import { NavLink } from "react-router-dom";
-import Logo from "../images/hero-bg.jpg";
-import Slider from "./Slider";
-import { useLocation } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../images/hero-bg.jpg';
+import Slider from './Slider';
+import { useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faUser,
   faMagnifyingGlass,
   faCartShopping,
-} from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
-import UserLogOut from "./UserLogOut";
-import { useDispatch, useSelector } from "react-redux";
-import { productsActions } from "../store/index";
-import { Badge } from "react-bootstrap";
+} from '@fortawesome/free-solid-svg-icons';
+import { useSelector, useDispatch } from 'react-redux'; 
+import { Badge, Navbar, Nav } from 'react-bootstrap';
+import UserLogOut from './UserLogOut';
+import { productsActions } from '../store/index'; 
+
 const userIcon = <FontAwesomeIcon icon={faUser} />;
 const cartIcon = <FontAwesomeIcon icon={faCartShopping} />;
 const search = <FontAwesomeIcon icon={faMagnifyingGlass} />;
 
-// eslint-disable-next-line react/prop-types
 function Header() {
+  const dispatch = useDispatch(); 
   const user = useSelector((state) => state.user);
   const arrayProducts = useSelector((state) => state.products.items);
   const cartQuant = useSelector((state) => state.cart.totalQuantity);
-  const dispatch = useDispatch();
   const location = useLocation();
+  const [searchText, setSearchText] = useState('');
+  const [searchProd, setSearchProd] = useState(false);
+  const [filtredProd, setFilterProd] = useState([]);
 
-  let background = false;
-  let heightHerou = {
-    minHeight: "100vh",
-  };
-  if (location.pathname === "/") {
-    background = true;
-  } else if (location.pathname !== "/") {
-    heightHerou = {
-      minHeight: "auto",
+    let background = false;
+    let heightHerou = {
+      minHeight: "100vh",
     };
-  }
+    if (location.pathname === "/") {
+      background = true;
+    } else if (location.pathname !== "/") {
+      heightHerou = {
+        minHeight: "auto",
+      };
+    }
 
   const computeLinkStyle = (path, exact = false) => {
     const isActive = exact
       ? location.pathname === path
       : location.pathname.startsWith(path);
-    return { color: isActive ? "#ffbe33" : "white" };
+    return { color: isActive ? '#ffbe33' : 'white' };
   };
 
-  const colorBackground = {
-    backgroundColor: "black",
-  };
-
-  const [searchText, setSearchText] = useState(" ");
-  const [searchProd, setSearchProd] = useState(false);
-  const [filtredProd, setFilterProd] = useState([]);
-  const searchClick = (e) => {
-    e.preventDefault();
-    setSearchProd(!searchProd);
-  };
   const handleChange = (e) => {
     setSearchText(e.target.value);
     if (arrayProducts) {
@@ -65,90 +58,46 @@ function Header() {
       setFilterProd(filtered);
     }
   };
+  
 
   function searchNow(e) {
-    e.preventDefault();
-    if (filtredProd.length > 0) {
-      console.log(filtredProd);
-      const products = filtredProd;
-      dispatch(productsActions.filtedProd(filtredProd));
-      //navigate(`/search/?${searchText}`);
-    }
-  }
- 
-
-  return (
-    <>
-      <div className="hero_area" style={heightHerou}>
-        {background && (
-          <div className="bg-box">
-            <img src={Logo} alt="" />
-          </div>
-        )}
-        <header className="header_section" style={colorBackground}>
-          <div className="container">
-            <nav className="navbar navbar-expand-lg custom_nav-container ">
+        e.preventDefault();
+        if (filtredProd.length > 0) {
+          console.log(filtredProd);
+          const products = filtredProd;
+          dispatch(productsActions.filtedProd(filtredProd));
+          //navigate(`/search/?${searchText}`);
+        }
+      }
+      return (
+        <>
+        <div className="hero_area" style={heightHerou}>
+          {location.pathname === '/' && (
+            <div className="bg-box">
+              <img src={Logo} alt="" />
+            </div>
+          )}
+          <Navbar expand="lg" className="header_section" bg="dark" variant="dark" >
+            <div className="container">
               <NavLink className="navbar-brand" to="/">
                 <span>Feane</span>
               </NavLink>
-
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className=""> </span>
-              </button>
-
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <ul className="navbar-nav  mx-auto ">
-                  <li className="nav-item">
-                    <NavLink
-                      className="nav-link"
-                      style={computeLinkStyle("/", true)}
-                      to="/"
-                      exact
-                    >
-                      Home <span className="sr-only">(current)</span>
-                    </NavLink>
-                  </li>
-
-                  <li className="nav-item">
-                    <NavLink
-                      className="nav-link"
-                      style={computeLinkStyle("/menu")}
-                      to="/menu"
-                    >
-                      Menu
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink
-                      to="/about"
-                      className="nav-link"
-                      style={computeLinkStyle("/about")}
-                    >
-                      About
-                    </NavLink>
-                  </li>
-
-                  <li className="nav-item">
-                    <NavLink
-                      className="nav-link"
-                      style={computeLinkStyle("/booktable")}
-                      to="/booktable"
-                    >
-                      Book Table
-                    </NavLink>
-                  </li>
-                </ul>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="navbar-nav mx-auto">
+                  <NavLink className="nav-link" style={computeLinkStyle('/', true)} to="/" exact>
+                    Home
+                  </NavLink>
+                  <NavLink className="nav-link" style={computeLinkStyle('/menu')} to="/menu">
+                    Menu
+                  </NavLink>
+                  <NavLink className="nav-link" style={computeLinkStyle('/about')} to="/about">
+                    About
+                  </NavLink>
+                  <NavLink className="nav-link" style={computeLinkStyle('/booktable')} to="/booktable">
+                    Book Table
+                  </NavLink>
+                </Nav>
                 <div className="user_option">
                   {!user.isLoggedIn && (
                     <NavLink to="/login" className="user_link">
@@ -157,36 +106,33 @@ function Header() {
                   )}
                   {user.isLoggedIn && <UserLogOut user={user} />}
                   <NavLink to="/shop" className="user_link">
-                  {cartIcon}
-                  
-                    {cartQuant > 0 && (
-                      <Badge bg="secondary">{cartQuant}</Badge>
-                    )}
-                    <span className="visually-hidden"></span>
+                    {cartIcon}
+                    {cartQuant > 0 && <Badge bg="secondary">{cartQuant}</Badge>}
                   </NavLink>
-
                   <form className="form-inline">
                     <button
-                      onClick={searchClick}
-                      className="btn  my-2 my-sm-0 nav_search-btn"
-                      type="submit"
+                      onClick={() => setSearchProd(!searchProd)}
+                      className="btn my-2 my-sm-0 nav_search-btn"
+                      type="button"
                     >
                       {search}
                       <i className="fa fa-search" aria-hidden="true"></i>
                     </button>
                   </form>
-
                   <a href="" className="order_online">
                     Order Online
                   </a>
                 </div>
-              </div>
-            </nav>
-            {searchProd && (
-              <nav
-                className="navbar nav_search navbar-light bg-light"
-                style={{ width: "30%", float: "right" }}
-              >
+              </Navbar.Collapse>
+            </div>
+          </Navbar>
+          {searchProd && (
+            <Navbar
+              bg="light"
+              className="nav_search"
+              style={{ width: '30%', float: 'right' }}
+            >
+              <div className="container">
                 <form className="form-inline">
                   <input
                     className="form-control mr-sm-2"
@@ -197,24 +143,20 @@ function Header() {
                     value={searchText}
                   />
                   <button
-                    onClick={(e) => {
-                      searchNow(e);
-                    }}
+                    onClick={searchNow}
                     className="btn btn-outline-success my-2 my-sm-0"
-                    type="submit"
+                    type="button"
                   >
-                    <NavLink to="search">Search</NavLink>
+                    Search
                   </button>
                 </form>
-              </nav>
-            )}
-          </div>
-        </header>
-
-        {background && <Slider />}
-      </div>
-    </>
-  );
-}
-
-export default Header;
+              </div>
+            </Navbar>
+          )}
+          {background && <Slider />}
+        </div>
+        </>
+      );
+    }
+    
+    export default Header;
